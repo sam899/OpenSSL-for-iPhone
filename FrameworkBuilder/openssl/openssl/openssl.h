cba34475a4615c58026eba6532bf7e4a481ecf3b
Stefan van den Oord <soord@mac.com>
2015-09-30 22:44:51 +0200

    Set up an Xcode project that turns the openssl static lib into a dynamic framework.

---
+++ FrameworkBuilder/openssl/openssl/openssl.h
@@ -0,0 +1,19 @@
+//
+//  openssl.h
+//  openssl
+//
+//  Created by Stefan on 30/09/15.
+//  Copyright © 2015 Stefan van den Oord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for openssl.
+FOUNDATION_EXPORT double opensslVersionNumber;
+
+//! Project version string for openssl.
+FOUNDATION_EXPORT const unsigned char opensslVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <openssl/PublicHeader.h>
+
+
